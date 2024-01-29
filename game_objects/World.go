32afbc792955e4905384d0ef8ff7ed469ada2e37
AaronM <tenix109@gmail.com>
2024-01-29 17:55:43 -0500

    some player and world set up

---
+++ game_objects/World.go
@@ -0,0 +1,34 @@
+components {
+  id: "WorldGenerator"
+  component: "/scripts/WorldGenerator.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "dirt_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/DirtBlock.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
